69585cc8fb6ec18a9c6755b8d106dd12284a01e3
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-11-18 02:31:45 +0000

    🐛 Fix user authentication bug

--- src/Pages/ProtectedRoute.js
+++ src/Pages/ProtectedRoute.js
@@ -1,12 +1,46 @@
+import { Box, CircularProgress } from "@mui/material";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import auth from "../firebase.init";
 
-import { Navigate } from 'react-router-dom';
-import auth from "../firebase.init"
-const ProtectedRoute = ({children}) =>{
-    if(auth){
-        return children;
-    } else {
-        return <Navigate to="/login" />
-    }
-}
+const ProtectedRoute = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const authState = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => authState();
+  }, []);
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
 
 export default ProtectedRoute;
